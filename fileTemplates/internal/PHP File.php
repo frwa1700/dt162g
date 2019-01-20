ed5b9d9891c720c7f98d7d3bb21541160223d3c6
fredrikwaldfelt <fredrikwaldfelt@fredriks-air.lan>
2019-01-20 23:40:58 +0100

    PS-2018.3.1 <fredrikwaldfelt@fredriks-air.lan Create baseRefactoring.xml, markdown.xml, project.default.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, laf.xml, web-browsers.xml, filetypes.xml, php.xml, colors.scheme.xml, databaseSettings.xml, databaseDrivers.xml, editor.xml, usageView.xml, applicationLibraries.xml, webServers.xml, debugger.xml, PHP File.php, updates.xml

---
+++ fileTemplates/internal/PHP File.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Course: DT0093G - Webbutveckling II
+ * Assignment: ${PROJECT_NAME}
+ * Author: Fredrik Waldfelt - frwa1700@student.miun.se
+ * Date: ${YEAR}-${MONTH}-${DAY}
+ * Filename: ${FILE_NAME}
+ * Function: 
+ */
